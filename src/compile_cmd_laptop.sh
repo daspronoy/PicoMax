9b5c124ed6d58f5f00f3f738ade04d7f2196a4f5
Pronoy Das <39770841+pronoyiiserk@users.noreply.github.com>
2025-05-14 22:45:37 -0400

    init

---
+++ src/compile_cmd_laptop.sh
@@ -0,0 +1,17 @@
+#compile_cmd.sh
+#!/bin/bash
+BASE=/mnt/c/Users/prono/Projects/PicoMax-1
+
+
+# -ffast-math
+# -funsafe-math-optimizations
+# -fno-signed-zeros
+# -ffinite-math-only
+# -fno-math-errno -fno-trapping-math
+
+#-L/usr/lib/x86_64-linux-gnu/hdf5/serial \
+# -I /usr/include/hdf5/serial \
+# -I ${BASE}/include/hdf5-1.14.3/src -I ${BASE}/include/hdf5 
+g++ -O3 -fopenmp -ffast-math \
+    -I ${BASE}/include/eigen-3.4.0\
+    -o picomax pmx.cpp pmx_io.cpp pmx_chi.cpp pmx_basis.cpp pmx_epm.cpp
