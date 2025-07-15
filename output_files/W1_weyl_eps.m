33eb7c3cc0ffc91f946ca57d0a2d0cc9fb180ed4
Pronoy Das <pronoyiiserk@gmail.com>
2025-07-15 08:39:55 -0400

    soc

--- output_files/W1_weyl_eps.m
+++ output_files/W1_weyl_eps.m
@@ -1,4 +1,4 @@
-filename = ['chi_11_G.dat'];
+filename = ['chi_11_G_soc.dat'];
 fid = fopen(filename);
 data= fread(fid,'double');
 % fclose(fid);
@@ -11,18 +11,19 @@ dfreq=0.02;
 nq=1;
 neps=9;
 ne = neps*(neps+1)/2;
-N = 3*3*ne*nq*nfreq;
-realepsij = permute(reshape(data(1:N),[nfreq ne nq 3 3]),[1 3 2 5 4]);
-imagepsij = permute(reshape(data(N+1:2*N),[nfreq ne nq 3 3]),[1 3 2 5 4]);
+NTSR=3;
+N = NTSR*NTSR*ne*nq*nfreq;
+realepsij = permute(reshape(data(1:N),[nfreq ne nq NTSR NTSR]),[1 3 2 5 4]);
+imagepsij = permute(reshape(data(N+1:2*N),[nfreq ne nq NTSR NTSR]),[1 3 2 5 4]);
 
 
 
 f=20;
-chiij = zeros([3*neps 3*neps nq]);   
+chiij = zeros([NTSR*neps NTSR*neps nq]);   
 for q=1:nq
     %lower triangle
-    for i=1:3
-        for j=1:3
+    for i=1:NTSR
+        for j=1:NTSR
             for m=1:neps
                 for n=1:m
                     idx = (m-1)*m/2+n;
@@ -33,8 +34,8 @@ for q=1:nq
         end
     end
     %upper triangle
-    for i=1:3
-        for j=1:3
+    for i=1:NTSR
+        for j=1:NTSR
             for m=1:neps
                 for n=1:m
                     chiij((j-1)*neps+n,(i-1)*neps+m,q) = conj(chiij((i-1)*neps+m,(j-1)*neps+n,q));
@@ -43,6 +44,10 @@ for q=1:nq
         end
     end
 end
-epsij = eye(3*neps)+chiij;
+epsij = eye(NTSR*neps)+chiij;
 [V,D]=eigenshuffle(epsij);
-D=sort(D,'descend','ComparisonMethod','real');
+diag_epsij=[];
+for i=1:NTSR*neps
+    diag_epsij=[diag_epsij;epsij(i,i)];
+end
+
--- output_files/untitled.m
+++ output_files/untitled.m
@@ -1,22 +1,22 @@
-filename = ['temp2.dat'];
+filename = ['band.dat'];
 fid = fopen(filename);
 data= fread(fid,'double');
 fclose(fid);
 
-npts=401;
+npts=31;
 eband=reshape(data(1:end,:),[80 npts]);
 set(gcf,'renderer','opengl', 'Position',  [500, 100, 900, 800])
 
 plot(1:npts,eband(1:end,:), 'Color','#505050','LineWidth',1.5);
 
-xticks([1 41 121 161 241 281 361 401]);
-xticklabels({"$\Gamma$","A","L","K","M", "L", "H","A"});
-
-
-ylabel("$\epsilon - \epsilon_F$ (eV)","Interpreter","latex");
-xline([41 121 161 241 281 361],'--');
-
-ylim([-2 1]);
-xlim([1 npts]);
+% xticks([1 41 121 161 241 281 361 401]);
+% xticklabels({"$\Gamma$","A","L","K","M", "L", "H","A"});
+% 
+% 
+% ylabel("$\epsilon - \epsilon_F$ (eV)","Interpreter","latex");
+% xline([41 121 161 241 281 361],'--');
+% 
+% ylim([-2 1]);
+% xlim([1 npts]);
 
 set(gca, 'TickLabelInterpreter', 'latex', 'FontSize', 20, 'LineWidth', 0.5);
