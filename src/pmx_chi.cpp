a6232f6d915599b4d1710154e44acdf7db31ad9d
Pronoy Das <das168@purdue.edu>
2025-07-17 01:25:47 -0400

    soc

--- src/pmx_chi.cpp
+++ src/pmx_chi.cpp
@@ -524,9 +524,9 @@ void chi_tensor(env &dat){
                                 if (i==0){
                                     orb_contribution = uvec_m[i].dot(v_orb);
                                 } else if (i==1) { 
-                                    orb_contribution = 0.5*(uvec1+im*uvec2).dot(v_orb);
+                                    orb_contribution = sqrt(0.5)*(uvec1+im*uvec2).dot(v_orb);
                                 } else {
-                                    orb_contribution = 0.5*(uvec1-im*uvec2).dot(v_orb);
+                                    orb_contribution = sqrt(0.5)*(uvec1-im*uvec2).dot(v_orb);
                                 }
                                 // Eigen::Vector3cd v_orb = (dat.lat.G[p] + K + Q/2 + dat.lat.G[m]/2).cast<std::complex<double>>();
                                 std::complex<double> soc_contribution = 0.0 * SF_SOC * uvec_m[i].dot(v_soc_cache[i_active]);
