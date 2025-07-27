a027b0aeb6fe25011292e53ee6be3bbe522f3bd7
Pronoy Das <pronoyiiserk@gmail.com>
2025-07-26 22:20:03 -0400

    soc

--- src/pmx_chi.cpp
+++ src/pmx_chi.cpp
@@ -536,7 +536,7 @@ void chi_tensor(env &dat){
                                     orb_contribution = uvecn.dot(v_orb);
                                 }
                                 // Eigen::Vector3cd v_orb = (dat.lat.G[p] + K + Q/2 + dat.lat.G[m]/2).cast<std::complex<double>>();
-                                std::complex<double> soc_contribution = uvec_m[i].dot(v_soc_cache[i_active]);
+                                std::complex<double> soc_contribution = 0.0 * uvec_m[i].dot(v_soc_cache[i_active]);
                                 // std::complex<double> orb_contribution = uvec_m[i].dot(v_orb);
                                 // Apply contributions
                                 int c_spin = c % 2;  // 0=up, 1=down
